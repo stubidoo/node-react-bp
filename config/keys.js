108837cea43e2c1f95dfd7ab9837da14a26c1abb
stubidoo <stuart.glass2@gmail.com>
2019-05-05 10:05:37 +0100

    users works

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://admin:yUHVKn7RwZWvMwa@ds149596.mlab.com:49596/wedding_invite'
+}
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+  .connect(db)
+  .then( () => console.log('MongoDB Connected'))
+  .catch( err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello'));
+
+//Use Routes
+app.use('/api/users', users);
+
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-// deded
